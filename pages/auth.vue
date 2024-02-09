61ebcb00b927525db9d19e91cb9c4f8d3eaa37ba
rof1yev <rofiyevdilshod@gmail.com>
2024-02-10 01:10:16 +0500

    hero section completed

---
+++ pages/auth.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>Auth Page</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,26 @@ useSeoMeta({
 </script>
 
 <template>
-  <!-- HERO SECTION -->
+  <!-- HERO SECTION start -->
   <section
-    class="flex items-center justify-between h-[100vh] pt-[10vh] gap-4 bg-gradient-to-t from-white dark:from-slate-900 to-blue-800"
+    class="flex items-center justify-between h-[100vh] pt-[10vh] gap-4 bg-gradient-to-t from-white dark:from-slate-900 to-blue-800 px-10"
   >
-    Home Page
+    <div>
+      <h1 class="text-5xl">
+        Move fast, stay aligned, and build better - together
+      </h1>
+      <p class="text-lg opacity-80">
+        The #1 software development tool used by agile teams
+      </p>
+
+      <NuxtLink to="/auth">
+        <UButton color="blue">Try it free</UButton>
+      </NuxtLink>
+    </div>
+    <NuxtImg src="/hero.webp" />
   </section>
+  <!-- HERO SECTION end -->
+
+  <!-- TAB SECTION start -->
+  <!-- TAB SECTION end -->
 </template>
